9952d37b55f9afc80c488d8c8750d9a3a5f47fcd
Chundi Liu <chundiliu@cs.toronto.edu>
2017-11-08 17:48:25 -0500

    working on tfrecords

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -22,13 +22,14 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
+from datasets import cdiscount
 
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
     'imagenet': imagenet,
     'mnist': mnist,
-    'cdiscount': cdiscount
+    'cdiscount': cdiscount,
 }
 
 
